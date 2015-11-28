ed281e14a60ef1ab6369dd5b8a11f669439638d2
Nalipp <nateplipp@gmail.com>
2015-11-28 16:16:37 +0900

    restrict user access and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only [:edit, :update, :show]
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -45,4 +46,10 @@ class UsersController < ApplicationController
       params.require(:user).permit(:username, :email, :password)
     end
 
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "you can only edit your own account"
+        redirect_to users_path
+      end
+    end
 end
