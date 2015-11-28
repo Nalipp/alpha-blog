e569f72d869195792dedc615fb20818cedc1141b
Nalipp <nateplipp@gmail.com>
2015-11-28 10:29:02 +0900

    add login logout funcitons

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ resources :users, except: [:new]
 root 'pages#home'
 get  'about', to: 'pages#about'
 get  'signup', to: 'users#new'
+
+get  'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+get  'logout', to: 'sessions#destroy'
 end
